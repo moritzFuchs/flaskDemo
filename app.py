00311cc3d46447f56b764f9000304ad81e7ad920
moritz <fuchsmo@in.tum.de>
2017-05-26 13:50:22 +0200

    Add first version of ticker app.

--- app.py
+++ app.py
@@ -1,14 +1,69 @@
-from flask import Flask, render_template, request, redirect
+'''
+Created on 24.05.2017
+
+@author: Moritz Fuchs
+'''
+import quandl
+from quandl.errors.quandl_error import NotFoundError
+from bokeh.charts import TimeSeries
+from bokeh.embed import components
+from flask import Flask, render_template, request, jsonify
+from pandas import DataFrame
+import pandas as pd
+from datetime import date, timedelta
+from dateutil.relativedelta import relativedelta
 
 app = Flask(__name__)
+# Set Quandl API key
+quandl.ApiConfig.api_key = "ahDPSmt3RPsTSSo5PT3_"
 
 @app.route('/')
-def main():
-  return redirect('/index')
-
 @app.route('/index')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
+def format_date_for_quandl(date):
+    return date.strftime("%Y-%m-%d")
+
+@app.route('/get_stock_plot', methods=['GET'])
+def get_stock_plot():
+
+    # Get Data for stock the user requests 
+    stock_keys = request.args.get("stock_names").split(",")
+    if len(stock_keys) == 0 :
+        return jsonify({"error": "Please enter a valid stock token."})
+    
+    stock_data = DataFrame()
+    error_list = []
+    for stock_key in stock_keys:
+        try:
+            data = quandl.get("WIKI/" + stock_key, start_date=format_date_for_quandl(date.today() + relativedelta(months=-1)), end_date=format_date_for_quandl(date.today()))
+        except NotFoundError:
+            error_list.append(stock_key)
+            continue
+        opening_data = data[["Adj. Open"]]
+        opening_data = opening_data.rename(columns={"Date":"Date", "Adj. Open": stock_key})
+        
+        # Save data to session
+        if stock_data.empty:
+            stock_data = opening_data
+        else:
+            stock_data = pd.concat([stock_data, opening_data], axis=1)
+            
+    
+    p = TimeSeries(stock_data, y=list(stock_data), title=", ".join(stock_keys), xlabel="", ylabel="Dollar")
+    p.legend.click_policy="hide"
+    
+    script, div = components(p)
+    
+    result = {"script": script, "div": div}
+    if len(error_list) > 0:
+        result["notFound"] = error_list
+    
+    return jsonify(result)  
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507, debug=True)
+    
+
+
---
+++ static/stockData.js
@@ -0,0 +1,59 @@
+var active_stocks = [];
+
+$(function() {
+    
+    $("#stock_names").tagsInput({
+       'height':'100px',
+       'width':'600px',
+       'interactive':true,
+       'defaultText':'Add a stock token (e.g. GOOG), then press \'\,\'',
+       'onAddTag':plotStocks,
+       'onRemoveTag':plotStocks,
+       'delimiter': [','],
+       'removeWithBackspace' : true,
+       'minChars' : 0,
+       'maxChars' : 0, // if not provided there is no limit
+       'placeholderColor' : '#666666'
+    });
+    
+    function plotStocks(elem, elem_tags) {
+       
+       if($("#stock_names").val() == "") {
+           return;
+       }
+       
+       
+       $("#bokeh_wrapper").append("<div id='overlay'><img src='static/loading.svg'></div>");
+       
+       $.getJSON("get_stock_plot", 
+        {stock_names : $("#stock_names").val()},
+        function(data) {
+            $("#error").remove();
+            $("#bokeh_wrapper").remove();
+            $("#content").append("<div id='bokeh_wrapper'>" + data.div + data.script + "</div>");
+            if (data.notFound != undefined) {
+            
+                $('.tag', elem_tags).each(function(index, tag) {
+                   if ($.inArray($(tag).text().substr(0,$(tag).text().length-3), data.notFound) != -1) {
+                       $(tag).removeClass("tag");
+                       $(tag).addClass("badTag");
+                   } 
+                });
+            
+                html = "<div id='error'>";
+                html += "The following stock tokens could not be found: ";
+                html += "<ul>";
+                $.each(data.notFound, function(index, value) {
+                   html += "<li>" + value + "</li>"; 
+                });
+                html += "</ul>";
+                html += "</div>";
+                //$(html).insertBefore("#bokeh_wrapper");
+                $("#error").fadeOut(3000);
+            }
+            $("#stock_name").val("");
+        }
+       );
+    }
+    
+});
